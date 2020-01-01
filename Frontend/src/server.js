f0324d6b7c2b60c0208f2243c22df65041c76c6d
RamonaN <ramona.nechita99@yahoo.ro>
2020-01-01 23:21:26 +0200

    upload page

--- Frontend/src/server.js
+++ Frontend/src/server.js
@@ -1,27 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-var multer  =   require('multer');
-var idUser="10Ab";
-var storage =   multer.diskStorage({
-  destination: function (req, file, callback) {
-    callback(null, './uploads');
-  },
-  filename: function (req, file, callback) {
-    callback(null, idUser+"-"+file.originalname );
-  }
-});
-var upload = multer({ storage : storage}).single('userPhoto');
-
- app.post('/api/photo',function(req,res){
-    upload(req,res,function(err) {
-        if(err) {
-            return res.end("Error uploading file.");
-        }
-        res.end("File is uploaded");
-    });
-}); 
 app.use(express.static('code'));
 app.use(express.static('images'))
-app.use(express.static('uploads'))
+
 app.listen(port, () => console.log(`listening on port ${port}!`));
