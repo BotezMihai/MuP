f3aa534d0726a02128010b646c0c93df9cb66d82
BotezMihai <mihai_b9812@yahoo.ro>
2020-01-26 23:55:58 +0200

    manage

--- Backend/controllers/youtube.js
+++ Backend/controllers/youtube.js
@@ -12,8 +12,8 @@ const Youtube = YoutubeModel(config, bd);
 
 const { google } = require('googleapis');
 const fetch = require('node-fetch');
-var youtubeV3 = google.youtube({ version: 'v3', auth: 'AIzaSyDKEXCYmy4rvAhOb8RV4R5Tyhntg4T3nwg' });
-var key = 'AIzaSyDKEXCYmy4rvAhOb8RV4R5Tyhntg4T3nwg';
+
+var key = 'AIzaSyARFBpQYS5QVNDB_Sgtzl_YvIdd4yUtmCk';
 var query = 'lady gaga';
 async function get_youtube_id(singer_name) {
 
@@ -28,6 +28,7 @@ async function search_new_song(res, id_melodie, id_petrecere) {
     var results_info_song = await config.query(`select * from melodii_user left join melodii on melodii_user.titlu_melodie=melodii.titlu 
     left join tag on tag.id_melodie=melodii.id where melodii.id=:id_melodie and melodii_user.id_petrecere=:id_petrecere`,
         { replacements: { id_melodie: id_melodie, id_petrecere: id_petrecere }, type: config.QueryTypes.SELECT, raw: true });
+        console.log(results_info_song);
     const URL_TOP_ARTISTS = encodeURI(`http://ws.audioscrobbler.com/2.0/?method=tag.gettopartists&tag=${results_info_song[0].tag}&api_key=4d7c7783c33be71fec00763625260dc8&format=json`);
     var results = await fetch(URL_TOP_ARTISTS);
     var json = await results.json();
@@ -37,6 +38,8 @@ async function search_new_song(res, id_melodie, id_petrecere) {
     for (let song of json.topartists.artist) {
         // console.log(song.name);
         var youtube = await get_youtube_id(song.name);
+        console.log(youtube);
+        break;
         // for (let element of youtube) {
         //     let result = results_youtube.filter(element => {
         //         return element.indexOf(element.videoId) !== -1;
@@ -51,7 +54,7 @@ async function search_new_song(res, id_melodie, id_petrecere) {
 
 }
 exports.get_new_song = async (req, res) => {
-    var id_petrecere = req.body.id_petrecere;
+    var id_petrecere = req.params.id_petrecere;
     console.log("?????????????????????????", id_petrecere);
     Playing.hasMany(Dansatori, { foreignKey: 'id_playing' });
     Dansatori.belongsTo(Playing, { foreignKey: 'id_playing' });
