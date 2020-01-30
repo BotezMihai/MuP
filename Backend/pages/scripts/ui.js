8cd155da4fe86bc02fd6b760fcbe39e43d206159
RamonaN <ramona.nechita99@yahoo.ro>
2020-01-30 12:37:15 +0200

    refactoring

--- Backend/pages/scripts/ui.js
+++ Backend/pages/scripts/ui.js
@@ -1,9 +1,11 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "300px";
+  var w = window.innerWidth;
+  if(w<450){
+      document.getElementById("mySidenav").style.width="100%";
   }
+  else{
+      document.getElementById("mySidenav").style.width="350px";
+  }}
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  
-  }
-
-  
+  document.getElementById("mySidenav").style.width = "0";
+}
